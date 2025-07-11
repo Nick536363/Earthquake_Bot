d2b93d93520bea9601c1c7c8c92c023e5252bcca
Nick536363 <134803463+Nick536363@users.noreply.github.com>
2025-07-11 13:10:41 +0300

    Добавил наработки работы в реальном времени

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import date
 from argparse import ArgumentParser
 from os import environ
 from dotenv import load_dotenv, find_dotenv
+from time import sleep
 
 
 def get_coords(place: str, apikey: str):
@@ -21,6 +22,7 @@ def get_coords(place: str, apikey: str):
     return lon, lat
 
 def get_data(starttime: str, endtime: str, latitude: int, longitude: int, maxradius: int):
+    data = []
     url = "https://earthquake.usgs.gov/fdsnws/event/1/query"
     params = {
         "format":"geojson",
@@ -34,23 +36,38 @@ def get_data(starttime: str, endtime: str, latitude: int, longitude: int, maxrad
     response = get(url=url, params=params)
     response.raise_for_status()
     for event in response.json()["features"]:
-        print(event["properties"]["type"])
-        print(event["properties"]["place"])
-        print(event["geometry"]["coordinates"])
-        print("\n\n")
+        data.append({
+            "place": event["properties"]["place"],
+            "coords": event["geometry"]["coordinates"]
+        })
+    return data
+
 
 
 def main():
+    # Получение данных для запроса
     load_dotenv(find_dotenv())
     yandex_api_key = environ["YANDEX_API"]
     parser = ArgumentParser()
     parser.add_argument("place", type=str, help="Место, в радиусе которого будут найдены землетрясения")
-    parser.add_argument("--maxradius", type=str, help="Максимальный радиус в котором будут найдены землетрясения", default=180)
-    parser.add_argument("--starttime", type=str, help="Дата (YYYY-MM-DD), от которой будут найдены землетрясения", default=date.today())
-    parser.add_argument("--endtime", type=str, help="Дата (YYYY-MM-DD), до которой будут найдены землетрясения", default=date.today())
+    parser.add_argument("--maxradius", type=str, help="Максимальный радиус в котором будут найдены землетрясения", default=50)
     args = parser.parse_args()
     longitude, latitude = get_coords(args.place, apikey=yandex_api_key)
-    get_data(args.starttime, args.endtime, latitude, longitude, args.maxradius)
+    data = get_data(date.today(), date.today(), latitude, longitude, args.maxradius)
+    events_count = len(data)
+    # Основной цикл
+    while 1:
+        new_data = get_data(args.starttime, date.today(), latitude, longitude, args.maxradius)
+        new_events = len(new_data)
+        print(f"Кол-во землятресений - {events_count}")
+        print(f"Кол-во землятресений (new) - {new_events}")
+        if new_events > events_count:
+            print("Новое землетрясение!")
+            print(new_data[len(new_data)-1])
+            data = new_data
+            events_count = new_events
+        sleep(3)
+
 
     
 if __name__ == "__main__":
