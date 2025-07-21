e7560c08e727f6c925390075071a4ac751d98f67
yatoro-asu <onetapw610@gmail.com>
2025-07-21 08:26:02 +0300

    Update uiquakebot.py

--- uiquakebot.py
+++ uiquakebot.py
@@ -4,16 +4,6 @@ from dotenv import load_dotenv, find_dotenv
 from os import environ
 import telebot
 
-
-def find_last_earthquakes(place: str, days_ago=1):
-    load_dotenv(find_dotenv())
-    yandex_api_key = environ["YANDEX_API"]
-    longitude, latitude = get_coords(place, apikey=yandex_api_key)
-    if not latitude or not longitude:
-        return None
-    last_earthquakes = get_earthquakes(f"{date.today().year}-{date.today().month}-{date.today().day-days_ago}", date.today(), latitude, longitude)
-    return last_earthquakes
-
 bot = telebot.TeleBot(os.environ["тг токен"])
 
 @bot.message_handler(commands=['earthquakes'])
