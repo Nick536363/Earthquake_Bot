965abe660420d76355686efbaf38e72bada1f581
Nick536363 <3383371nik@gmail.com>
2025-07-22 16:40:25 +0200

    Убрал дефолтное значение аргумента в функции получения землетрясений

--- earthquakes_info.py
+++ earthquakes_info.py
@@ -61,7 +61,7 @@ def get_earthquakes(starttime: str, endtime: str, latitude: int, longitude: int)
     return data
 
 
-def find_last_earthquakes(lat: float, lon: float, days_ago=1):
+def find_last_earthquakes(lat: float, lon: float, days_ago: int):
     # Получение всех данных и формирование сообщения для отправки
     last_earthquakes = get_earthquakes(f"{date.today().year}-{date.today().month}-{date.today().day-days_ago}", date.today(), lat, lon)
     return last_earthquakes
